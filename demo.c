57c5d579c49774c7d90cc92202e48e1f1d2abe8f
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2018-12-13 17:39:53 -0500

    added function definitions.

--- demo.c
+++ demo.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node *{
+	struct node *link;
+	int info;
+}
+
+struct node *creatList(struct node *last);
+void displayList(struct node *last);
+struct node *insertInEmptyList(struct node *last, int data);
+struct node *insertInBeginning(struct node *last, int data);
+struct node *insertAtEnd(struct node *last, int data);
+struct node *deleteNode(struct node *last, int data);
+
 int main()
 {
+	int choice, data, item;
+	struct node *last = NULL;
    printf("Hello, World!");
    return 0;
 }
