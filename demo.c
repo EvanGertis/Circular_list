c6df28ae38c8ac99fd9e0ee9adbd5fc2268ce4c2
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2018-12-13 17:45:54 -0500

    added function bodies.

--- demo.c
+++ demo.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node *{
+struct node {
 	struct node *link;
 	int info;
-}
+};
 
-struct node *creatList(struct node *last);
+struct node *createList(struct node *last);
 void displayList(struct node *last);
 struct node *insertInEmptyList(struct node *last, int data);
 struct node *insertInBeginning(struct node *last, int data);
@@ -20,3 +20,32 @@ int main()
    printf("Hello, World!");
    return 0;
 }
+
+struct node *createList(struct node *last){
+	
+	return last;
+}/*End createList*/
+
+void displayList(struct node *last){
+	
+}/*End displayList*/
+
+struct node *insertInEmptyList(struct node *last, int data){
+	
+	return last;
+}/*End insertInEmptyList*/
+
+struct node *insertInBeginning(struct node *last, int data){
+		
+	return last;
+}/*End insertInBeginning*/
+
+struct node *insertAtEnd(struct node *last, int data){
+	
+	return last;
+}/*End insertAtEnd*/
+
+struct node *deleteNode(struct node *last, int data){
+	
+	return last;
+}/*End delete node*/
